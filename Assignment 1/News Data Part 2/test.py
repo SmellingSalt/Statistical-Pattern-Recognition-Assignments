cfbdab9036e08d2094c702d5b5d4e32c4f5da4c8
SawanSM <ssmahara96@gmail.com>
2020-02-10 00:31:47 -0600

    Clean Up

--- Assignment 1/News Data Part 2/test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from scipy.stats import dirichlet
-quantiles = np.array([0.1, 0.3, 0.6])  # specify quantiles
-alpha = np.array([0.4, 5, 15])  # specify concentration parameters
-print(dirichlet.pdf(quantiles, alpha))
