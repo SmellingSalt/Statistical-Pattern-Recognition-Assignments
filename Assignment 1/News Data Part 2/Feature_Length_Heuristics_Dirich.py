638b91f92070d124be3738bb956e7203fce4e73b
SawanSM <ssmahara96@gmail.com>
2020-02-10 22:29:18 -0600

    Final

--- Assignment 1/News Data Part 2/Feature_Length_Heuristics_Dirich.py
+++ Assignment 1/News Data Part 2/Feature_Length_Heuristics_Dirich.py
@@ -9,18 +9,26 @@ import numpy as np
 import scipy.io
 from Dirichlet_Classifierv2 import Dirich
 
-step=20
-vocab_length= range(20, 2500,step)
+step=100
+vocab_length= range(50, 2000,step)
 our_score=np.zeros((len(vocab_length),1))
+y_true=[]#np.zeros((len(vocab_length),1))
+y_pred=[]
 in_built_score=our_score
 itr=0;
 for n in vocab_length:
-    print("------------------------------------------------------------------------Iteration: ",itr+1," of ",len(vocab_length),"------------------------------------------------------------------------\n")
-    [our_score[itr], in_built_score[itr]]=Dirich(n)
+    print("-------------------------Iteration: ",itr+1," of ",len(vocab_length),"-------------------------\n")
+    [our_score[itr], in_built_score[itr],temp1,temp2]=Dirich(n)
+    
+    y_pred.append(temp1)
+    y_true.append(temp2)
     vocab_size=n
     itr=itr+1   
     print("Vocabulary Length: ",vocab_size," Accuracy: ",our_score[itr-1],"\n")
     
-scipy.io.savemat('Acurracies/our_score.mat', mdict={'our_score':(our_score)})
-scipy.io.savemat('Acurracies/in_built_score.mat', mdict={'in_built_score':(in_built_score)})
-scipy.io.savemat('Acurracies/vocab_size.mat', mdict={'vocab_size':(vocab_size)})
+    scipy.io.savemat('Acurracies/our_score.mat', mdict={'our_score':(our_score)})
+    scipy.io.savemat('Acurracies/in_built_score.mat', mdict={'in_built_score':(in_built_score)})
+    scipy.io.savemat('Acurracies/vocab_size.mat', mdict={'vocab_size':(vocab_size)})
+    
+    scipy.io.savemat('Acurracies/y_pred.mat', mdict={'y_pred':(y_pred)})
+    scipy.io.savemat('Acurracies/y_true.mat', mdict={'y_true':(y_true)})
